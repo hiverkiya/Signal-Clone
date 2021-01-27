7e9c9c9e329cc0e7311224540402f9c788d9dc65
Himanshu Verkiya <hiverkiya@gmail.com>
2021-01-28 03:21:01 +0530

    Adding dependencies

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAggPwAa_qA717LIAOAap4fHY7Gg1Bc-mk",
+    authDomain: "signalcloned.firebaseapp.com",
+    projectId: "signalcloned",
+    storageBucket: "signalcloned.appspot.com",
+    messagingSenderId: "629136115868",
+    appId: "1:629136115868:web:3431b008a8ef54ebcdc559"
+  };
